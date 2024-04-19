983173d2f1d290d8dc9ed760605c004a5efbee60
조인화 <ihnhwa8362@naver.com>
2024-04-19 09:28:35 +0900

    [BE] setting: Spring Project Start

---
+++ backend/RelicKing/src/main/java/com/SevenEleven/RelicKing/RelicKingApplication.java
@@ -0,0 +1,13 @@
+package com.SevenEleven.RelicKing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RelicKingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RelicKingApplication.class, args);
+	}
+
+}
