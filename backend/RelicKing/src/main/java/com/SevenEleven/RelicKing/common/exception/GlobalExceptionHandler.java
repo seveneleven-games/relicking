f33e1d0ae3f65bac7882341b04d37af503a0edc2
조인화 <ihnhwa8362@naver.com>
2024-04-29 21:46:01 +0900

    [BE] feat: MethodArgumentNotValidException 예외 처리 handler 구현

--- backend/RelicKing/src/main/java/com/SevenEleven/RelicKing/common/exception/GlobalExceptionHandler.java
+++ backend/RelicKing/src/main/java/com/SevenEleven/RelicKing/common/exception/GlobalExceptionHandler.java
@@ -1,6 +1,8 @@
 package com.SevenEleven.RelicKing.common.exception;
 
+import org.springframework.context.support.DefaultMessageSourceResolvable;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -17,5 +19,13 @@ public class GlobalExceptionHandler {
         return ResponseEntity
             .status(e.getExceptionType().getStatus())
             .body(new ResponseFail(e.getExceptionType().getStatus(), e.getExceptionType().getMessage()));
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<?> validExceptionHandler(MethodArgumentNotValidException e) {
+        String message = e.getAllErrors().get(0).getDefaultMessage();
+        return ResponseEntity
+            .status(e.getStatusCode().value())
+            .body(new ResponseFail(e.getStatusCode().value(), message));
     }
 }
