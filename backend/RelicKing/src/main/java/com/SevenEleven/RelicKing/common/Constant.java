b71d319e10d6a01a6f27947a32d5748dc9bf3fcb
조인화 <ihnhwa8362@naver.com>
2024-04-30 15:11:06 +0900

    [BE] style: 상수 위치 변경 (#8)

--- backend/RelicKing/src/main/java/com/SevenEleven/RelicKing/common/Constant.java
+++ backend/RelicKing/src/main/java/com/SevenEleven/RelicKing/common/Constant.java
@@ -1,6 +1,10 @@
 package com.SevenEleven.RelicKing.common;
 
-public interface JWTProperties {
+public interface Constant {
+
+	int MAX_STAGE = 3;
+
+	// jwt
 	Long ACCESS_TOKEN_EXPIRATION_TIME = 864000000L;    // 10일	FIXME : 시간 짧게 조정하고 reissue 로직 제대로 구현하기
 	Long REFRESH_TOKEN_EXPIRATION_TIME = 864000000L;    // 10일
 	String ACCESS_TOKEN_PREFIX = "Bearer ";
