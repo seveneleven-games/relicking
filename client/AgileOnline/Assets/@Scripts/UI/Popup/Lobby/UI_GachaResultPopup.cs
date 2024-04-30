7646edf570fc3694fe85551cefe20d57260d5a41
김형규 <dhe0814@naver.com>
2024-04-30 14:01:34 +0900

    [CT] Feat : 가챠 결과 팝업 추가
    - 가챠 결과 팝업 기초 구현

---
+++ client/AgileOnline/Assets/@Scripts/UI/Popup/Lobby/UI_GachaResultPopup.cs
@@ -0,0 +1,64 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UI_GachaResultPopup : UI_Popup
+{
+    #region Enum
+    enum EGameObjects
+    {
+        ContentObjet,
+        GachaResultListObject,
+    }
+
+    enum EButtons
+    {
+        CloseButton,
+    }
+
+    enum ETexts
+    {
+
+    }
+
+    #endregion
+
+    public void OnDestroy()
+    {
+        if (Managers.Game != null)
+            Managers.Game.OnResourcesChanged -= Refresh;
+    }
+
+    // 초기 세팅
+    public override bool Init()
+    {
+        if (base.Init() == false)
+            return false;
+
+        #region Object Bind
+
+        BindObject(typeof(EGameObjects));
+        BindButton(typeof(EButtons));
+
+        GetButton((int)EButtons.CloseButton).gameObject.BindEvent(OnClickCloseButton);
+       
+        #endregion
+
+        Managers.Game.OnResourcesChanged += Refresh;
+        Refresh();
+
+        return true;
+    }
+
+    // 갱신
+    void Refresh()
+    {
+
+    }
+
+    void OnClickCloseButton()
+    {
+        Debug.Log("CloseGachaResultPopup");
+        Managers.UI.ClosePopupUI(this);
+    }
+}
