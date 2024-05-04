4534f8bf889a61c31c38bdc15c9886c2bf1dc6dc
조인화 <ihnhwa8362@naver.com>
2024-05-04 18:10:14 +0900

    [BE] fix: import 추가 (#16)

--- backend/RelicKing/src/main/java/com/SevenEleven/RelicKing/service/MemberService.java
+++ backend/RelicKing/src/main/java/com/SevenEleven/RelicKing/service/MemberService.java
@@ -7,6 +7,7 @@ import java.util.Arrays;
 import java.util.Date;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
