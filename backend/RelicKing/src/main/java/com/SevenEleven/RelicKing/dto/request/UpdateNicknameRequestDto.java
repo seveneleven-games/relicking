762e0b7852ed2744dd85e11300ee733cc2a556b7
조인화 <ihnhwa8362@naver.com>
2024-05-05 14:36:17 +0900

    [BE] fix: 오류 메세지 및 swagger 설명 수정 (#17)

--- backend/RelicKing/src/main/java/com/SevenEleven/RelicKing/dto/request/UpdateNicknameRequestDto.java
+++ backend/RelicKing/src/main/java/com/SevenEleven/RelicKing/dto/request/UpdateNicknameRequestDto.java
@@ -13,7 +13,7 @@ public class UpdateNicknameRequestDto {
 	@NotBlank(message = "닉네임은 필수 입력 값입니다.", groups = NotBlankGroup.class)
 	@Pattern(
 		regexp = "^[가-힣a-zA-Z0-9!@#$%^&*()_+=-]{1,12}$",
-		message = "닉네임은 영문, 숫자, 특수문자만 가능하며 12자 이내여야 합니다.",
+		message = "닉네임은 한글, 영문, 숫자, 특수문자만 가능하며 12자 이내여야 합니다.",
 		groups = PatternGroup.class
 	)
 	private String nickname;
