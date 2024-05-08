a3c9d585db324c28d34c5b876ef4ced6c996cea7
Dongyoung <doyokim75@gmail.com>
2024-05-08 14:23:56 +0900

    [BE] feat: Inventory API(#2)
    - 변경 내용 : GET Inventories 보유한 유물 정보만 반환하도록 변경

--- backend/RelicKing/src/main/java/com/SevenEleven/RelicKing/service/InventoryService.java
+++ backend/RelicKing/src/main/java/com/SevenEleven/RelicKing/service/InventoryService.java
@@ -1,14 +1,10 @@
 package com.SevenEleven.RelicKing.service;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
-import com.SevenEleven.RelicKing.common.Constant;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import com.SevenEleven.RelicKing.dto.model.MemberRelicDTO;
 import com.SevenEleven.RelicKing.dto.request.RelicChangeRequestDTO;
 import com.SevenEleven.RelicKing.dto.response.InventoryResponseDTO;
 import com.SevenEleven.RelicKing.entity.Member;
@@ -33,21 +29,22 @@ public class InventoryService {
 	public InventoryResponseDTO getInventoryInfo(Member member) {
 		List<MemberRelic> memberRelicList = memberRelicRepository.findByMember(member);
 
-		MemberRelicDTO[] memberRelicDTOS = new MemberRelicDTO[Constant.THE_NUMBER_OF_RELICS];
-		Arrays.setAll(memberRelicDTOS, i -> new MemberRelicDTO(i + 1, 0, 0, 0));
-
-		memberRelicList.forEach(memberRelic -> {
-			memberRelicDTOS[memberRelic.getRelicNo() - 1] =
-					new MemberRelicDTO(
-							memberRelic.getRelicNo(),
-							memberRelic.getLevel(),
-							memberRelic.getExp(),
-							memberRelic.getSlot());
-		});
+		// Todo 주석 지우기
+		// MemberRelicDTO[] memberRelicDTOS = new MemberRelicDTO[Constant.THE_NUMBER_OF_RELICS];
+		// Arrays.setAll(memberRelicDTOS, i -> new MemberRelicDTO(i + 1, 0, 0, 0));
+		//
+		// memberRelicList.forEach(memberRelic -> {
+		// 	memberRelicDTOS[memberRelic.getRelicNo() - 1] =
+		// 			new MemberRelicDTO(
+		// 					memberRelic.getRelicNo(),
+		// 					memberRelic.getLevel(),
+		// 					memberRelic.getExp(),
+		// 					memberRelic.getSlot());
+		// });
 
 		return InventoryResponseDTO.builder()
 			.currentClassNo(member.getCurrentClassNo())
-			.myRelicList(new ArrayList<>(List.of(memberRelicDTOS)))
+			.myRelicList(memberRelicList.stream().map(MemberRelic::entityToDTO).toList())
 			.build();
 	}
 
