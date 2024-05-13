4c7140efea701713327eb16ef044ab399e7ca742
hyoseon <wink2090@naver.com>
2024-05-13 12:57:22 +0900

    CT feat: 어드레서블 수정

--- client/AgileOnline/Assets/@Scripts/Utils/Define.cs
+++ client/AgileOnline/Assets/@Scripts/Utils/Define.cs
@@ -97,7 +97,7 @@ public static class Define
     public static int SPEED_RATE = 600;
     public static int RANGE = 9;
 
-    public static int ADDRESSABLE_COUNT = 123;
+    public static int ADDRESSABLE_COUNT = 125;
 
     #endregion
 
