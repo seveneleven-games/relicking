da08c9f9e8c07599b1709e54d46df9319e93896e
전근렬 <hm04137@naver.com>
2024-05-22 15:19:48 +0900

    [CT] 시연을 위한 수치 조정 복구

--- client/AgileOnline/Assets/@Scripts/UI/Popup/Ingame/UI_InGamePopup.cs
+++ client/AgileOnline/Assets/@Scripts/UI/Popup/Ingame/UI_InGamePopup.cs
@@ -22,7 +22,7 @@ public class UI_InGamePopup : UI_Popup
 
     private TextMeshProUGUI _timerText;
     private TextMeshProUGUI _bossName;
-    private float _remainingTime = 15f;
+    private float _remainingTime = 30f;
     private TemplateData _templateData;
 
 
